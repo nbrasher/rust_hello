07ce6cc1da64c84606d711c79a8fc3d07e7fff13
N B <nbrasher@cross-entropy.com>
2022-05-24 12:06:58 -0500

    feat: ugly but working LRU cache

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{ collections::HashMap, vec::Vec };
 
 struct Node {
+    key: usize,
     value: usize,
     prev: Option<usize>,
     next: Option<usize>,
@@ -19,35 +20,59 @@ impl LRUCache {
         let mut cache = Vec::with_capacity(capacity + 2);
         
         // Pseudo-head and tail for LL implementation, head will be at index 0, tail at 1
-        cache.push(Node{ value: 0, next: Some(1), prev: None });
-        cache.push(Node{ value: 0, next: None, prev: Some(0) });
+        cache.push(Node{ key: 0, value: 0, next: Some(1), prev: None });
+        cache.push(Node{ key: 0, value: 0, next: None, prev: Some(0) });
 
         LRUCache { capacity, size: 0, map, cache }
     }
-    pub fn insert(&mut self, key: usize, value: usize) {
-        if self.map.contains_key(&key) {
-            let ix = self.map.get(&key).unwrap();
+    pub fn insert(&mut self, key: usize, value: usize) -> () {
+        if let Some(ix) = self.map.get(&key) {
             self.cache[*ix].value = value;
         } else {
-            // TODO - handle at capacity case
+            if self.size >= self.capacity {
+                // Update pointers to remove least-recently used node
+                let to_update = self.cache[1].prev.unwrap();
 
-            // Create new node, insert to cache
-            let head_next = self.cache[0].next;
-            self.cache.push(Node{ value, next: head_next, prev: Some(0) });
+                // Remove old key from map
+                let old_key = self.cache[to_update].key;
+                self.map.remove(&old_key);
 
-            // Update pointers, p-head.next and p-head.next.prev -> new node
-            let head_next = self.cache[0].next.unwrap();
-            self.cache[0].next = Some(self.size + 2);
-            self.cache[head_next].prev = Some(self.size + 2);
+                let prev_ix = self.cache[to_update].prev.unwrap();
+                let next_ix = self.cache[to_update].next.unwrap();
 
-            self.map.insert(key, self.size + 2);
-            self.size += 1;
+                self.cache[prev_ix].next = Some(next_ix);
+                self.cache[next_ix].prev = Some(prev_ix);
+                
+                // Insert new node at head
+                let head_next = self.cache[0].next.unwrap();
+                self.cache[to_update] = Node{ key, value, next: Some(head_next), prev: Some(0) };
+                self.cache[0].next = Some(to_update);
+                self.cache[head_next].prev = Some(to_update);
+                self.map.insert(key, to_update);
+            } else {
+                // Create new node, insert to cache
+                let head_next = self.cache[0].next.unwrap();
+                self.cache.push(Node{ key, value, next: Some(head_next), prev: Some(0) });
+
+                // Update pointers, p-head.next and p-head.next.prev -> new node
+                let new_ix = self.size + 2;
+                self.cache[0].next = Some(new_ix);
+                self.cache[head_next].prev = Some(new_ix);
+
+                self.map.insert(key, new_ix);
+                self.size += 1;
+            }
         }
     }
-    pub fn get(&self, key: usize) -> Option<usize> {
-        match self.map.get(&key) {
-            Some(ix) => Some(self.cache[*ix].value),
-            None => None,
+    pub fn get(&mut self, key: usize) -> Option<usize> {
+        if let Some(ix) = self.map.get(&key) {
+            let head_next = self.cache[0].next.unwrap();
+            self.cache[0].next = Some(*ix);
+            self.cache[head_next].prev = Some(*ix);
+
+            Some(self.cache[*ix].value)
+        } else {
+            None
         }
     }
 }
@@ -57,7 +82,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lru_size () {
+    fn test_lru_size() {
         let lru = LRUCache::new(5);
 
         assert_eq!(lru.capacity, 5);
@@ -74,5 +99,28 @@ mod tests {
         assert_eq!(lru.get(5), Some(5));
         assert_eq!(lru.get(3), Some(3));
         assert_eq!(lru.get(1), None);
+    }
+
+    #[test]
+    fn test_lru_drop() {
+        let mut lru = LRUCache::new(3);
+        lru.insert(1, 1);
+        lru.insert(2, 5);
+        lru.insert(3, 3);
+        lru.insert(4, 4);
+
+        // First inserted key is dropped
+        assert_eq!(lru.get(1), None);
+
+        // Other keys are correct
+        assert_eq!(lru.get(2), Some(5));
+        assert_eq!(lru.get(3), Some(3));
+        assert_eq!(lru.get(4), Some(4));
+
+        lru.insert(5, 6);
+
+        // Last access key is dropped
+        assert_eq!(lru.get(2), None);
+        assert_eq!(lru.get(5), Some(6));
     }
 }
