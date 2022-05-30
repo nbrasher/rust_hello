043491ac88e846e249ca8424727175aae3a99559
N B <nbrasher@cross-entropy.com>
2022-05-29 21:28:53 -0500

    fix: better pointers with enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,38 @@
 use std::{ collections::HashMap, vec::Vec };
 
+#[derive(Copy, Clone)]
+enum Ptr {
+    Head,
+    Tail,
+    Index(usize),
+}
+
 struct Node {
     key: usize,
     value: usize,
-    prev: Option<usize>,
-    next: Option<usize>,
+    prev: Ptr,
+    next: Ptr,
 }
 
 pub struct LRUCache {
     capacity: usize,
     size: usize,
+    head: Ptr,
+    tail: Ptr,
     map: HashMap<usize, usize>,
     cache: Vec<Node>
 }
 
 impl LRUCache {
     pub fn new(capacity: usize) -> LRUCache {
-        let map = HashMap::new();
-        let mut cache = Vec::with_capacity(capacity + 2);
-        
-        // Pseudo-head and tail for LL implementation, head will be at index 0, tail at 1
-        cache.push(Node{ key: 0, value: 0, next: Some(1), prev: None });
-        cache.push(Node{ key: 0, value: 0, next: None, prev: Some(0) });
-
-        LRUCache { capacity, size: 0, map, cache }
+        LRUCache { 
+            capacity,
+            size: 0,
+            head: Ptr::Tail,
+            tail: Ptr::Head, 
+            map: HashMap::new(),
+            cache: Vec::with_capacity(capacity),
+        }
     }
     pub fn insert(&mut self, key: usize, value: usize) -> () {
         if let Some(ix) = self.map.get(&key) {
@@ -31,33 +40,47 @@ impl LRUCache {
         } else {
             if self.size >= self.capacity {
                 // Update pointers to remove least-recently used node
-                let to_update = self.cache[1].prev.unwrap();
+                if let Ptr::Index(to_update) = self.tail {
 
-                // Remove old key from map
-                let old_key = self.cache[to_update].key;
-                self.map.remove(&old_key);
+                    // Remove old key from map
+                    let old_key = self.cache[to_update].key;
+                    self.map.remove(&old_key);
 
-                let prev_ix = self.cache[to_update].prev.unwrap();
-                let next_ix = self.cache[to_update].next.unwrap();
-
-                self.cache[prev_ix].next = Some(next_ix);
-                self.cache[next_ix].prev = Some(prev_ix);
-                
-                // Insert new node at head
-                let head_next = self.cache[0].next.unwrap();
-                self.cache[to_update] = Node{ key, value, next: Some(head_next), prev: Some(0) };
-                self.cache[0].next = Some(to_update);
-                self.cache[head_next].prev = Some(to_update);
-                self.map.insert(key, to_update);
+                    // TODO - figure out how to better update these pointers
+                    match self.cache[to_update].prev {
+                        Ptr::Head => self.head = self.cache[to_update].next,
+                        Ptr::Tail => (),
+                        Ptr::Index(ix) => self.cache[ix].next = self.cache[to_update].next,
+                    };
+                    match self.cache[to_update].next {
+                        Ptr::Head => (),
+                        Ptr::Tail => self.tail = self.cache[to_update].prev,
+                        Ptr::Index(ix) => self.cache[ix].prev = self.cache[to_update].prev,
+                    };
+                    
+                    // Insert new node at head
+                    self.cache[to_update] = Node{ key, value, next: self.head, prev: Ptr::Head };
+                    self.cache[0].next = Ptr::Index(to_update);
+                    if let Ptr::Index(head_next) = self.head {
+                        self.cache[head_next].prev = Ptr::Index(to_update);
+                    }
+                    self.map.insert(key, to_update);
+                }
             } else {
                 // Create new node, insert to cache
-                let head_next = self.cache[0].next.unwrap();
-                self.cache.push(Node{ key, value, next: Some(head_next), prev: Some(0) });
+                self.cache.push(Node{ key, value, next: self.head, prev: Ptr::Head });
 
                 // Update pointers, p-head.next and p-head.next.prev -> new node
-                let new_ix = self.size + 2;
-                self.cache[0].next = Some(new_ix);
-                self.cache[head_next].prev = Some(new_ix);
+                let new_ix = self.size;
+                
+                // If list was non-empty, update previous head, else update tail
+                if let Ptr::Index(head_next) = self.head {
+                    self.cache[head_next].prev = Ptr::Index(new_ix);
+                } else {
+                    self.tail = Ptr::Index(new_ix);
+                }
+                
+                self.head = Ptr::Index(new_ix);
 
                 self.map.insert(key, new_ix);
                 self.size += 1;
@@ -66,11 +89,13 @@ impl LRUCache {
     }
     pub fn get(&mut self, key: usize) -> Option<usize> {
         if let Some(ix) = self.map.get(&key) {
-            let head_next = self.cache[0].next.unwrap();
-            self.cache[0].next = Some(*ix);
-            self.cache[head_next].prev = Some(*ix);
-
-            Some(self.cache[*ix].value)
+            if let Ptr::Index(head_next) = self.head {
+                self.head = Ptr::Index(*ix);
+                self.cache[head_next].prev = Ptr::Index(*ix);
+                Some(self.cache[*ix].value)
+            } else {
+                None
+            }
         } else {
             None
         }
