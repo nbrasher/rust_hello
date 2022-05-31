46a7581d06abdc2605d19db122052306ce4287be
N B <nbrasher@cross-entropy.com>
2022-05-31 15:55:12 -0500

    feat: update to use references, remove Copy trait req

--- src/lib.rs
+++ src/lib.rs
@@ -7,32 +7,32 @@ enum Ptr {
     Index(usize),
 }
 
-struct Node<K, V> {
-    key: K,
-    value: V,
+struct Node<'a, K, V> {
+    key: &'a K,
+    value: &'a V,
     prev: Ptr,
     next: Ptr,
 }
 
-pub struct LRUCache<K, V> {
+pub struct LRUCache<'a, K, V> {
     capacity: usize,
     size: usize,
     head: Ptr,
     tail: Ptr,
-    map: HashMap<K, usize>,
-    cache: Vec<Node<K, V>>
+    map: HashMap<&'a K, usize>,
+    cache: Vec<Node<'a, K, V>>
 }
 
-impl <K, V> LRUCache<K, V> 
-where K: Eq + Hash + Copy, V: Copy
+impl <'a, K, V> LRUCache<'a, K, V> 
+where K: Eq + Hash
 {
-    pub fn new(capacity: usize) -> LRUCache<K, V> {
+    pub fn new(capacity: usize) -> LRUCache<'a, K, V> {
         LRUCache { 
             capacity,
             size: 0,
             head: Ptr::Tail,
             tail: Ptr::Head, 
-            map: HashMap::<K, usize>::new(),
+            map: HashMap::<&'a K, usize>::new(),
             cache: Vec::<Node<K, V>>::with_capacity(capacity),
         }
     }
@@ -56,7 +56,7 @@ where K: Eq + Hash + Copy, V: Copy
         self.head = Ptr::Index(to_move);
         self.cache[to_move].prev = Ptr::Head;
     }
-    pub fn insert(&mut self, key: K, value: V) -> () {
+    pub fn insert(&mut self, key: &'a K, value: &'a V) -> () {
         if let Some(ix) = self.map.get(&key) {
             let ins_ix = *ix;
             self.move_to_head(ins_ix);
@@ -87,7 +87,7 @@ where K: Eq + Hash + Copy, V: Copy
             }
         }
     }
-    pub fn get(&mut self, key: K) -> Option<V> {
+    pub fn get(&mut self, key: &'a K) -> Option<&'a V> {
         match self.map.get(&key) {
             Some(ix) => {
                 let ret_ix = *ix;
@@ -114,53 +114,53 @@ mod tests {
     #[test]
     fn test_lru_put_get() {
         let mut lru = LRUCache::new(5);
-        lru.insert(5, 1);
-        lru.insert(5, 5);
-        lru.insert(3, 3);
+        lru.insert(&5, &1);
+        lru.insert(&5, &5);
+        lru.insert(&3, &3);
 
-        assert_eq!(lru.get(5), Some(5));
-        assert_eq!(lru.get(3), Some(3));
-        assert_eq!(lru.get(1), None);
+        assert_eq!(lru.get(&5), Some(&5));
+        assert_eq!(lru.get(&3), Some(&3));
+        assert_eq!(lru.get(&1), None);
     }
 
     #[test]
     fn test_ins_drop() {
         let mut lru = LRUCache::new(3);
-        lru.insert(1, 1);
-        lru.insert(2, 5);
-        lru.insert(3, 3);
-        lru.insert(4, 4);
+        lru.insert(&1, &1);
+        lru.insert(&2, &5);
+        lru.insert(&3, &3);
+        lru.insert(&4, &4);
 
         // First inserted key is dropped
-        assert_eq!(lru.get(1), None);
+        assert_eq!(lru.get(&1), None);
 
         // Other keys are correct
-        assert_eq!(lru.get(2), Some(5));
-        assert_eq!(lru.get(3), Some(3));
-        assert_eq!(lru.get(4), Some(4));
+        assert_eq!(lru.get(&2), Some(&5));
+        assert_eq!(lru.get(&3), Some(&3));
+        assert_eq!(lru.get(&4), Some(&4));
 
-        lru.insert(5, 6);
+        lru.insert(&5, &6);
 
         // Last access key is dropped
-        assert_eq!(lru.get(2), None);
-        assert_eq!(lru.get(5), Some(6));
+        assert_eq!(lru.get(&2), None);
+        assert_eq!(lru.get(&5), Some(&6));
     }
     
     #[test]
     fn test_get_drop() {
         let mut lru = LRUCache::new(3);
-        lru.insert("key1", 1);
-        lru.insert("key2", 2);
-        lru.insert("key3", 3);
+        lru.insert(&"key1", &1);
+        lru.insert(&"key2", &2);
+        lru.insert(&"key3", &3);
 
-        assert_eq!(lru.get("key1"), Some(1));
+        assert_eq!(lru.get(&"key1"), Some(&1));
 
         // Should drop "key2" since "key1" was just read above
-        lru.insert("key4", 4);
+        lru.insert(&"key4", &4);
 
-        assert_eq!(lru.get("key1"), Some(1));
-        assert_eq!(lru.get("key2"), None);
-        assert_eq!(lru.get("key3"), Some(3));
-        assert_eq!(lru.get("key4"), Some(4));
+        assert_eq!(lru.get(&"key1"), Some(&1));
+        assert_eq!(lru.get(&"key2"), None);
+        assert_eq!(lru.get(&"key3"), Some(&3));
+        assert_eq!(lru.get(&"key4"), Some(&4));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use rust_hello::LRUCache;
 
 fn main() {
     let mut lru = LRUCache::new(3);
-    lru.insert("key1", 1);
-    lru.insert("key2", 2);
-    lru.insert("key3", 3);
+    lru.insert(&"key1", &1);
+    lru.insert(&"key2", &2);
+    lru.insert(&"key3", &3);
 
-    println!("Value at key1 is {}\n", lru.get("key1").unwrap());
+    println!("Value at key1 is {}\n", lru.get(&"key1").unwrap());
 
-    lru.insert("key4", 4);
+    lru.insert(&"key4", &4);
 
-    for key in ["key1", "key2", "key3", "key4"] {
+    for key in [&"key1", &"key2", &"key3", &"key4"] {
         match lru.get(key) {
             None => println!("Key {} has been dropped", key),
             Some(value) => println!("Value at {} is {}", key, value),
